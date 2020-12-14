9546904cecd480b6b306d55aee2cb90f2a230612
qky1412 <qky1412@boostfield.com>
2020-12-14 14:18:45 +0800

    Modify timeslider

--- Source/BMTimeSlider.swift
+++ Source/BMTimeSlider.swift
@@ -10,8 +10,8 @@ import UIKit
 
 open class BMTimeSlider: UISlider {
     override open func trackRect(forBounds bounds: CGRect) -> CGRect {
-        let trackHeight: CGFloat = 2
-        let position = CGPoint(x: 0, y: 14)
+        let trackHeight: CGFloat = 14
+        let position = CGPoint(x: 0, y: 6)
         let customBounds = CGRect(origin: position, size: CGSize(width: bounds.size.width, height: trackHeight))
         super.trackRect(forBounds: customBounds)
         return customBounds
@@ -19,7 +19,7 @@ open class BMTimeSlider: UISlider {
     
     override open func thumbRect(forBounds bounds: CGRect, trackRect rect: CGRect, value: Float) -> CGRect {
         let rect = super.thumbRect(forBounds: bounds, trackRect: rect, value: value)
-        let newx = rect.origin.x - 10
+        let newx = rect.origin.x
         let newRect = CGRect(x: newx, y: 0, width: 30, height: 30)
         return newRect
     }
